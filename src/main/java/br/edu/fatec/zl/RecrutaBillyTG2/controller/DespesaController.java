6a396e02913ffe32a1756c1b5a96c9089626ccca
lant-silva <luiz.antoniocruz2008@hotmail.com>
2024-07-26 02:02:39 -0300

    correção

---
+++ src/main/java/br/edu/fatec/zl/RecrutaBillyTG2/controller/DespesaController.java
@@ -0,0 +1,179 @@
+package br.edu.fatec.zl.RecrutaBillyTG2.controller;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.fatec.zl.RecrutaBillyTG2.model.Despesa;
+import br.edu.fatec.zl.RecrutaBillyTG2.persistence.DespesaDao;
+import br.edu.fatec.zl.RecrutaBillyTG2.persistence.GenericDao;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class DespesaController {
+	
+	@Autowired
+	GenericDao gDao;
+	
+	@Autowired 
+	DespesaDao dDao;
+	
+	@RequestMapping(name = "despesas", value = "/despesas", method = RequestMethod.GET)
+	public ModelAndView despesasGet(@RequestParam Map<String, String> allRequestParam, ModelMap model, HttpSession session) {
+		String nivelAcesso = (String) session.getAttribute("nivelAcesso");
+		String erro = "";
+		String saida = "";
+		
+		List<Despesa> despesas = new ArrayList<>();
+		Despesa d = null;
+		
+		try {
+			String cmd = allRequestParam.get("cmd");
+			String codigo = allRequestParam.get("codigo");
+			String filtro = allRequestParam.get("filtro");
+			
+			if(cmd != null) {
+				if(cmd.contains("alterar")) {
+					d = new Despesa();
+					d.setCodigo(Integer.parseInt(codigo));
+					d = buscarDespesa(d);
+				}else {
+					if(cmd.contains("excluir")) {
+						d = new Despesa();
+						d.setCodigo(Integer.parseInt(codigo));
+						saida = excluirDespesa(d);
+						d = null;
+					}
+				}
+				despesas = listarDespesas(Integer.parseInt(filtro));
+			}
+		} catch(ClassNotFoundException | SQLException e) {
+			erro = e.getMessage();
+		}finally {
+			if(nivelAcesso == null || !nivelAcesso.equals("admin")) {
+				saida = "Você não possui acesso para visualizar esta página.";
+			}
+			model.addAttribute("erro", erro);
+			model.addAttribute("saida", saida);
+			model.addAttribute("despesa", d);
+			model.addAttribute("despesas", despesas);
+		}
+		return new ModelAndView("despesas");
+	}
+	
+	@RequestMapping(name = "despesas", value = "/despesas", method = RequestMethod.POST)
+	public ModelAndView despesasPost(@RequestParam Map<String, String> allRequestParam, ModelMap model, HttpSession session) {
+		String cmd = allRequestParam.get("botao");
+		String codigo = allRequestParam.get("codigo");
+		String nome = allRequestParam.get("nome");
+		String dataInicio = allRequestParam.get("data");
+		String dataVencimento = allRequestParam.get("dataVencimento");
+		String valor = allRequestParam.get("valor");
+		String tipo = allRequestParam.get("tipo");
+		String estado = allRequestParam.get("estado");
+		String filtro = allRequestParam.get("filtro");
+		
+		String saida = "";
+		String erro = "";
+		
+		Despesa d = new Despesa();
+		List<Despesa> despesas = new ArrayList<>();	
+		
+		if (cmd != null && !cmd.isEmpty() && cmd.contains("Limpar")) {
+			d = null;
+		} else if (!cmd.contains("Listar")) {
+			d.setCodigo(Integer.parseInt(codigo));
+		}
+		
+		if(cmd.contains("Cadastrar") || cmd.contains("Alterar")) {
+			d.setCodigo(Integer.parseInt(codigo));
+			d.setNome(nome);
+			d.setData(Date.valueOf(dataInicio));
+			d.setDataVencimento(Date.valueOf(dataVencimento));
+			d.setValor(Float.parseFloat(valor));
+			d.setTipo(tipo);
+			d.setEstado(estado);
+		}
+		try {
+			if(cmd.contains("Cadastrar")) {
+				saida = cadastrarDespesa(d);
+				d = null;
+			}
+			if(cmd.contains("Alterar")) {
+				saida = alterarDespesa(d);
+				d = null;
+			}
+			if(cmd.contains("Excluir")) {
+				saida = excluirDespesa(d);
+				d = null;
+			}
+			if(cmd.contains("Buscar")) {
+				d = buscarDespesa(d);
+				if(d==null) {
+					erro = "Despesa não encontrada";
+				}
+			}
+			if(cmd.contains("Listar")) {
+				despesas = listarDespesas(Integer.parseInt(filtro));
+			}
+		} catch(ClassNotFoundException | SQLException e) {
+			erro = e.getMessage();
+		} finally {
+			model.addAttribute("saida", saida);
+			model.addAttribute("erro", erro);
+			model.addAttribute("despesa", d);
+			model.addAttribute("despesas", despesas);
+		}
+		return new ModelAndView("despesas");
+	}
+
+	private String cadastrarDespesa(Despesa d) throws ClassNotFoundException, SQLException {
+		String saida = dDao.iudDespesa("I", d);
+		return saida;
+	}
+
+	private String alterarDespesa(Despesa d) throws ClassNotFoundException, SQLException {
+		String saida = dDao.iudDespesa("U", d);
+		return saida;
+	}
+
+	private String excluirDespesa(Despesa d) throws ClassNotFoundException, SQLException {
+		String saida = dDao.iudDespesa("D", d);
+		return saida;
+	}
+
+	private Despesa buscarDespesa(Despesa d) throws ClassNotFoundException, SQLException {
+		d = dDao.findBy(d);
+		return d;
+	}
+
+	private List<Despesa> listarDespesas(int filtro) throws ClassNotFoundException, SQLException {
+		List<Despesa> despesas = new ArrayList<>();
+		List<Despesa> auxDespesas = new ArrayList<>();
+		despesas = dDao.findAll();
+		
+		// Aplicação do filtro
+		if(filtro != 0) {
+			for(Despesa d : despesas) {
+				if(d.getData().getMonth() + 1 == filtro) {
+					auxDespesas.add(d);
+				}
+			}
+		}
+		if (!auxDespesas.isEmpty()){
+			return auxDespesas;
+		}else {			
+			return despesas;
+		}
+	}
+}
